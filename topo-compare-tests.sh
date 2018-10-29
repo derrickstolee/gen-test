3392f6c0d0bb2e904de634a5839f374208bb4641
Derrick Stolee <dstolee@microsoft.com>
2018-10-29 14:27:15 +0000

    test topo-order compare in both directions
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- topo-compare-tests.sh
+++ topo-compare-tests.sh
@@ -32,4 +32,24 @@ do
 			| grep -oE '[^ ]+$')"
 	done
 
+	GIT_TEST_OLD_PAINT=1 GIT_TR2_PERFORMANCE=$LOGFILE $TESTDIR/git/git log --topo-order -10 $mergeA..$mergeB >/dev/null
+
+	echo "$repo	$mergeB	$mergeA	OLD	$(grep "key:num_walked_explore" $LOGFILE \
+		| sed "s/:/ /g" \
+		| grep -oE '[^ ]+$')"
+
+	for v in 0 1 2 3 4 
+	do
+		cp .git/objects/info/commit-graph.$v .git/objects/info/commit-graph
+
+		LOGFILE=$TESTDIR/topo-compare-$repo-$v.txt
+		rm -f $LOGFILE
+
+		GIT_TR2_PERFORMANCE=$LOGFILE $TESTDIR/git/git log --topo-order -10 $mergeA..$mergeB >/dev/null
+
+		echo "$repo	$mergeB	$mergeA	$v	$(grep "key:num_walked_explore" $LOGFILE \
+			| sed "s/:/ /g" \
+			| grep -oE '[^ ]+$')"
+	done
+
 done <$TESTDIR/test-merges.txt
