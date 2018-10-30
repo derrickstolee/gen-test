fa52732234eb9740cde8c4908f3afd0106c586f1
Derrick Stolee <dstolee@microsoft.com>
2018-10-30 10:39:37 +0000

    add clone-repos.sh

---
+++ clone-repos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone git@github.com:derrickstolee/git.git --branch reach-perf
+
+git clone git@github.com:curl/curl
+git clone git@github.com:electron/electron
+git clone git@github.com:FFmpeg/FFmpeg
+git clone git@github.com:eclipse/jgit
+git clone git@github.com:JuliaLang/julia
+git clone git@github.com:jetbrains/kotlin
+git clone git@github.com:torvalds/linux
+git clone git@github.com:odoo/odoo
+git clone git@github.com:openssl/openssl
+git clone git@github.com:apple/swift
+git clone git@github.com:tensorflow/tensorflow
+git clone git@github.com:microsoft/TypeScript
+
+git clone https://gerrit.googlesource.com/gerrit
+git clone https://android.googlesource.com/platform/frameworks/base/ android-base
+git clone https://chromium.googlesource.com/chromium/src chromium
+
+
